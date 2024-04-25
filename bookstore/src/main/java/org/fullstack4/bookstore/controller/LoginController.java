9e6f6ca45ad842a6b7981e4ee3a72b0403fa829a
jy <juyoun919@gmail.com>
2024-04-25 19:15:11 +0900

    replace- login css , login ui

--- bookstore/src/main/java/org/fullstack4/bookstore/controller/LoginController.java
+++ bookstore/src/main/java/org/fullstack4/bookstore/controller/LoginController.java
@@ -1,22 +1,28 @@
 package org.fullstack4.bookstore.controller;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Log4j2
 @Controller
 @RequestMapping(value="/login")
-@RequiredArgsConstructor
 public class LoginController {
-    //    private final BbsServiceIf bbsServiceIf;
-//    private final BbsReplyServiceIf bbsReplyServiceIf;
+
     @GetMapping("/login")
-    public void view() {
-        log.info("===============================");
-        log.info("LoginController >> login");
-        log.info("===============================");
+    public void loginGet() {
+        log.info("로그인 겟");
     }
+    @PostMapping("/login")
+    public void loginPost() {
+        log.info("로그인 포스트");
+    }
+
+    @PostMapping("/logout")
+    public void logout() {
+        log.info("로그아웃");
+    }
+
 }
