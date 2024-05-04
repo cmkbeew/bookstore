ccc36b03c08b69e1b896c2b20b5529b0f68a9f43
jy <juyoun919@gmail.com>
2024-05-05 01:32:21 +0900

    [자료실]파일 다운로드

--- bookstore/src/main/java/org/fullstack4/bookstore/controller/AdminProductController.java
+++ bookstore/src/main/java/org/fullstack4/bookstore/controller/AdminProductController.java
@@ -8,10 +8,12 @@ import org.fullstack4.bookstore.dto.ProductPageResponseDTO;
 import org.fullstack4.bookstore.service.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
 import java.util.List;
